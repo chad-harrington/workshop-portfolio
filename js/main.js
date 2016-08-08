a4e09496ce0e0aa65bef7f8904218e8142ccb3c5
chad-harrington <chad@chadharrington.net>
2016-08-08 16:38:59 -0700

    Update markup, css, and js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 (function(){
 
+    // Extend jQuery with 'animateCSS' funciton
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
+
 
 
 }());
