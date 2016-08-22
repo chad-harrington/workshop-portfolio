a0969077f160aa7df516b6559ee358b4db83beb3
chad-harrington <chad@chadharrington.net>
2016-08-22 11:17:24 -0700

    Add navbar toggle animation

--- js/main.js
+++ js/main.js
@@ -10,11 +10,26 @@
         }
     });
 
+    // add 'open' class when navbar-toggle is opened
+    $('#navbar').on('show.bs.collapse', function() {
+        $(this)
+            .prev('.navbar-header')
+            .find('.navbar-toggle')
+            .addClass('open');
+    });
+    // remove 'open' class when navbar-toggle is closed
+    $('#navbar').on('hide.bs.collapse', function() {
+        $(this)
+            .prev('.navbar-header')
+            .find('.navbar-toggle')
+            .removeClass('open');
+    });
+
     // toggle skill item open/closed
     $('.skill-item-info button').on('click', function() {
         var $container = $(this).closest('.skill-item-info');
         $container.toggleClass('open');
-    })
+    });
 
 
 }());
